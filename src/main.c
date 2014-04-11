8f7075a88814368468c096a70f431a6c7f645c24
utnso <utnso@utnso.(none)>
2014-04-10 22:45:24 -0300

    Creo el proyecto y agrego un archivo que imprime a los integrantes

---
+++ src/main.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : main.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Integrantes del grupo:");
+	puts("\t** Tobias Sandler");
+	return EXIT_SUCCESS;
+}
