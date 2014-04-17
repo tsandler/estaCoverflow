f5b96fbf50dec5d2e759f62a2a0d404cda596dbd
Gonzalo Weiss <gonzaweiss@gmail.com>
2014-04-16 22:13:49 -0300

    Agregue comentarios

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("Integrantes del grupo: ");
+	puts("Integrantes del grupo: "); /*Colocar el nombre de cada uno a continuacion*/
 	puts("\t** Tobias Sandler");
 	puts("\t** Gonzalo Weiss");
 	puts("\t** Danila Gallo");
