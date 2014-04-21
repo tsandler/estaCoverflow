3270cb850a1a4931bd8961d47cbf4efa307283c2
Tobias Sandler <toby.s92@gmail.com>
2014-04-21 13:40:13 -0300

    Subo las librerias del parser y elimine un par de carpetas que estaban de mas

--- UMV/Debug/src/UMV.d
+++ UMV/Debug/src/UMV.d
@@ -1,7 +1,4 @@
 src/UMV.d: ../src/UMV.c \
- /home/utnso/workspace/tp-2014-1c-out-of-range/commons/libs/manejoStack.h \
- /home/utnso/workspace/tp-2014-1c-out-of-range/commons/commons/string.h
+ /home/utnso/workspace/tp-2014-1c-out-of-range/commons/libs/manejoStack.h
 
 /home/utnso/workspace/tp-2014-1c-out-of-range/commons/libs/manejoStack.h:
-
-/home/utnso/workspace/tp-2014-1c-out-of-range/commons/commons/string.h:
--- kernel/Debug/src/kernel.d
+++ kernel/Debug/src/kernel.d
@@ -1,7 +1,4 @@
 src/kernel.d: ../src/kernel.c \
- /home/utnso/workspace/tp-2014-1c-out-of-range/commons/libs/manejoDelPCB.h \
- /home/utnso/workspace/tp-2014-1c-out-of-range/commons/commons/string.h
+ /home/utnso/workspace/tp-2014-1c-out-of-range/commons/libs/manejoDelPCB.h
 
 /home/utnso/workspace/tp-2014-1c-out-of-range/commons/libs/manejoDelPCB.h:
-
-/home/utnso/workspace/tp-2014-1c-out-of-range/commons/commons/string.h:
