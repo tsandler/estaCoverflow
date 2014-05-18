2c5e847c324bbc9e7658972a1075986c4d3af6e4
Macarena Lepera <lepera.macarena@gmail.com>
2014-05-18 18:19:27 -0300

    Subo funcion destruir_segmentos

--- UMV/src/UMV.c
+++ UMV/src/UMV.c
@@ -10,46 +10,19 @@
 
 #include "functions.h"
 
-int main(){
+
+int main(int argc, char** argv){
 	logs= log_create("log","UMV.c",0,LOG_LEVEL_TRACE);
-	t_config *config = config_create("config.cnf");
+	if (argc < 2){
+		log_error(logs, "No se envio ningun parametro");
+		log_destroy(logs);
+		return 0;
+	}
+	t_config *config = config_create(argv[1]);
 	tamanioUMV = config_get_int_value(config, "tamanioUMV");
 
 
 	log_destroy(logs);
 	config_destroy(config);
 	return 0;
-}
-
-/* Funcion que crea y agrega un segmento a la lista de segmentos, segun pid */
-int crear_agregar_segmento(int pidInt, int tamanio){
-
-	tablaSegUMV *unElem = malloc(sizeof(tablaSegUMV));
-	int nroSeg=0;
-	char* pid = (char*)string_itoa(pidInt);
-	t_list *listaSeg;
-
-	unElem->tamanioSegmento= tamanio;
-	unElem->dirLogica= obtener_dir_logica(tamanio);
-	unElem->dirFisica = obtener_dir_fisica(tamanio, unElem->dirLogica);
-	if(dictionary_is_empty(tablaPidSeg) || !(dictionary_has_key(tablaPidSeg, pid))){
-		unElem->idSegmento=1;
-		listaSeg = list_create();
-		list_add(listaSeg, (void*)unElem);
-		dictionary_put(tablaPidSeg, pid, (void*)listaSeg);
-		if(dictionary_has_key(tablaPidSeg,pid))
-			log_info(logs,"El nuevo campo pid/lista se agregó correctamente");
-		else
-			log_error(logs,"El nuevo campo pid/lista no se agregó al diccionario");
-	}else if(dictionary_has_key(tablaPidSeg, pid)){
-		listaSeg = dictionary_get(tablaPidSeg,pid);
-		nroSeg = obtener_nroSeg(listaSeg);
-		unElem->idSegmento=nroSeg;
-		list_add_in_index(listaSeg,0,(void*)unElem);
-		if( (nroSeg-1) == obtener_nroSeg(listaSeg) )
-			log_info(logs,"El nuevo segmento se agregó correctamente a la lista");
-		else
-			log_error(logs,"El nuevo segmento no se agregó a la lista correspondiente");
-	}
-	return unElem->dirLogica;
 }
