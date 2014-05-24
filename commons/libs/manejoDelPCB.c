4973b5e272cd3c365036fcfdf6314760b79c474a
Matias Rapaport <rapaport.matias@gmail.com>
2014-05-24 11:36:02 -0300

    Agragre la estructura del Pcb por primera vez

--- commons/libs/manejoDelPCB.c
+++ commons/libs/manejoDelPCB.c
@@ -7,14 +7,68 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "../parser/metadata_program.h"
 #include "manejoDelPCB.h"
 
-registroPCB pcb;
+registroPCB crearNuevoPCB (){
 
-void inicializar(){
-	pcb.primera_instruccion = 1;
+	registroPCB pcb;
+
+	//INICIALIZO LAS VARIABLES DEL NUEVO PCB
+	pcb.pid = identificadorUnicoDelPCB();
+
+	return pcb;
 }
 
-void mostrar(){
-	printf("%d", pcb.primera_instruccion);
+
+
+
+int calcularPesoPCB (t_medatada_program program ){
+
+	int peso=0;
+
+
+	peso = program.cantidad_de_etiquetas + (3 * program.cantidad_de_funciones) + program.instrucciones_size;
+
+	return peso;
 }
+
+registroPCB llenarPCB(registroPCB pcb, t_medatada_program program, int stack, int codigo){
+
+
+
+		//SEGMENTO STACK
+		//Deberia igualarlo a la posicion donde comienza el stack, pero nunca se mueve
+		pcb.cursor_stack = stack;
+
+		//Este puntero recorre el segmento del stack.
+		pcb.tamanio_contexto = stack;
+		pcb.tamanio_contexto = 0;
+
+		//SEGMENTO DE CODIGO
+		 //Numero de intrucciones
+		pcb.tamanio_indice_codigo = program.instrucciones_size;
+
+		pcb.program_counter = 0;
+
+		//Puntero a primera intruccion a ejecutar en el segmento. guarda el
+		//el inicio del segmento de Codigo
+		pcb.segemento_codigo = codigo;
+		pcb.indece_codigo = codigo;
+
+		//SEGMENTO DE ETIQUETAS, FUNCIONES Y PROCEDIMEINTO
+		// aca dijo alien que debo usar una funcion metadata loca que me
+		//devuelve el lugar del indice de etiquetas. eh, pero al pcb no
+		//le importa, en generar.
+
+		//Pido la memoria necesaria para craer el vector de intruccion a ejecutar
+		pcb.tamanio_indice_etiquetas_funciones = program.etiquetas_size;
+		pcb.indice_etiquetas_funciones = program.etiquetas;
+		pcb.puntero_etiquetas_funciones = 0;
+
+		//HEAP
+
+
+	return pcb;
+}
+
--- commons/libs/manejoDelPCB.h
+++ commons/libs/manejoDelPCB.h
@@ -8,18 +8,45 @@
 #ifndef MANEJODELPCB_H_
 #define MANEJODELPCB_H_
 
+int ultimoIdentificador = 0;
+
+int identificadorUnicoDelPCB()
+{
+	ultimoIdentificador =+ 1;
+	return ultimoIdentificador;
+}
+
+
 typedef struct registroPCB{
-	int identificador_unico;
-	int tamanio_contexto; //tamaño del contexto
-	int program_counter; //indica que instruccion a ejecutar y la inicializa el parser
-	//A partir de aca lo da el parser
-	int primera_instruccion;
-	int indice_etiquetas_funciones; //indice de etiquetas y funciones serializado
-	int indice_codigo; //indice de codigo serializado
-	int tamanioIndice_etiquetas_funciones; //tamaño del indice de etiquetas y funciones
-	int tamanioIndice_codigo; //tamaño del indice del codigo
+    int pid;
+
+	//SEGMENTO DEL STACK
+	int segemento_stack; // puntero que siemrpe apunta al inicio del segmento del stack
+	int cursor_stack; // es el puntero que recorre el stack
+	int tamanio_contexto; //es un valor calculable en base a la diferencia del puntero del stack y contexto.
+
+	//SEGMENTO DE CODIGO
+	int segemento_codigo; //puntero que apunta simepre al inicio del segemento del codigo.
+	int indece_codigo; //  puntero que se mueve en el segmento codigo.
+	int tamanio_indice_codigo ; //tamaño del indice del codigo
+	t_intructions* instrucciones;	// array que guarda en cada componenete [longitud, desplazamiento]
+	int program_counter; //recorre el arrar del indice de codigo.
+
+	//SEGMENTO DE ETIQUETA, FUNCIONES Y PROCEDIMIENTOS
+
+	char* indice_etiquetas_funciones; //indice de etiquetas y funciones serializado
+	int puntero_etiquetas_funciones;
+	int tamanio_indice_etiquetas_funciones; //tamaño del indice de etiquetas y funciones
+
+	//HEAP....
+	int puntero_heap;
+
+
+
 }registroPCB;
 
+registroPCB crearNuevoPCB();
+registroPCB llenarPCB();
 void inicializar();
 void mostrar();
 
