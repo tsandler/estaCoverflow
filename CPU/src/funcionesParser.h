108691655086a4fb4d83657de65adb071ede32ef
Tobias Sandler <toby.s92@gmail.com>
2014-05-25 00:40:27 -0300

    Olvide subir el archivo funcionesParser.h actualizado

--- CPU/src/funcionesParser.h
+++ CPU/src/funcionesParser.h
@@ -19,8 +19,13 @@ void asignar(t_puntero direccion_variable, t_valor_variable valor );
 t_valor_variable obtenerValorCompartida(t_nombre_compartida variable);
 t_valor_variable asignarValorCompartida(t_nombre_compartida variable, t_valor_variable valor);
 void irAlLabel(t_nombre_etiqueta etiqueta);
+void llamarSinRetorno(t_nombre_etiqueta etiqueta);
+void llamarConRetorno(t_nombre_etiqueta etiqueta, t_puntero donde_retornar);
+void finalizar();
+void retornar(t_valor_variable retorno);
 void imprimir(t_valor_variable valor_mostrar);
 void imprimirTexto(char* texto);
+void entradaSalida(t_nombre_dispositivo dispositivo, int tiempo);
 void wait(t_nombre_semaforo identificador_semaforo);
 void signal(t_nombre_semaforo identificador_semaforo);
 
