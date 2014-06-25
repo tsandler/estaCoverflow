187455d41aabe45fccdeb25d862a2d486177cadf
hernansson <hernan.sslive.com>
2014-06-25 01:21:20 -0300

    arreglo de  libs pcb

---
+++ kernel/src/funcionesPCB.h
@@ -0,0 +1,44 @@
+/*
+ * manejoDelPCB.h
+ *
+ *  Created on: 24/05/2014
+ *      Author: utnso
+ */
+
+#ifndef MANEJODELPCB_H_
+#define MANEJODELPCB_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../parser/metadata_program.h"
+#include "sockets.h"
+#include "../commons/log.h"
+#include "../commons/config.h"
+//#include <kernel/src/planificador_largo_plazo.h>
+
+
+typedef struct registroPCB{
+    int pid;
+
+	int segmento_stack;
+	int cursor_stack;
+	int tamanio_contexto;
+
+	int segmento_codigo;
+	int tamanio_indice_codigo ;
+	int indice_codigo;
+	int program_counter;
+
+	int indice_etiquetas_funciones;
+	int puntero_etiquetas_funciones;
+	int tamanio_indice_etiquetas_funciones;
+
+	int retrasoIO;
+	int peso;
+	int fd;
+
+}registroPCB;
+
+registroPCB* armarPCB(char* program, int fd);
+
+#endif /* MANEJODELPCB_H_ */
