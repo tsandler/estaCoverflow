0e9c50ae55af288677076e95722501a2c24cf65d
Tobias Sandler <toby.s92@gmail.com>
2014-07-17 21:46:37 -0300

    CPU arreglado

--- CPU/src/CPU.c
+++ CPU/src/CPU.c
@@ -76,7 +76,7 @@ int main(int argc, char** argv){
 		ejecutando = 1; //Si se llama la senial SIGUSR1 espera a que concluya el quantum
 		while (quantum > cont && !systemCall){
 			pc = pcb->program_counter;
-			sleep(2);
+
 			char* sentencia = recibir_sentencia();
 			analizadorLinea(strdup(sentencia), &functions, &kernel_functions);
 
