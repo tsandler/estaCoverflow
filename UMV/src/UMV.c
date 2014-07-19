515ae1da164fa0d000231828da48b234632d7ac3
Macarena Lepera <lepera.macarena@gmail.com>
2014-07-19 17:32:46 -0300

    Dump

--- UMV/src/UMV.c
+++ UMV/src/UMV.c
@@ -15,7 +15,7 @@ sem_t mutexOpera;
 
 int main(int argc, char** argv){
 
- 	logs = log_create("log","UMV.c",1,LOG_LEVEL_TRACE);
+ 	logs = log_create("log","UMV.c",0,LOG_LEVEL_TRACE);
 	if (argc < 2){
 		log_error(logs, "No se envio ningun parametro");
 		log_destroy(logs);
